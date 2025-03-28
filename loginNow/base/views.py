d136397359c51d6407d9aac488a35dd4c94c7475
unknown <DedeRap>
2025-03-28 23:02:36 +0700

    Update28/3/2025

--- loginNow/base/views.py
+++ loginNow/base/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
 from django.contrib.auth.models import auth
+from django.contrib.auth import logout
 
 
 @login_required
+@never_cache
 def home(request):
     return render(request, "home.html", {})
 
@@ -19,6 +22,14 @@ def authView(request):
     else:
         form = UserCreationForm()
     return render(request, "registration/signup.html", {"form" :form})
+
+def custom_logout(request):
+    logout(request)
+    response = redirect('base:login')  # Ganti dengan nama URL login Anda
+    response['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response['Pragma'] = 'no-cache'
+    response['Expires'] = '0'
+    return response
 
 def logout(request):
     auth.logout(request)
